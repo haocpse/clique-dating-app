f4fe737f63ba40408c20c73e8f8ee5f8695c0fc6
haocp <haocpse184107@fpt.edu.vn>
2026-02-20 20:39:08 +0700

    [CLI] init project

---
+++ src/main/java/com/haocp/clique/CliqueApplication.java
@@ -0,0 +1,13 @@
+package com.haocp.clique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haocp/clique/CliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haocp.clique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
